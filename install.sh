f34a9b015271a968e6a611e303bd6b78b43e448b
AmirHosseinChoghaei <127476407+amirhosseinchoghaei@users.noreply.github.com>
2025-06-08 14:18:11 +0330

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+opkg update
+
+opkg install coreutils-sleep
+
+mkdir /usr/local/
+mkdir /usr/local/LED/
+
+cd /usr/local/LED/
+
+wget https://raw.githubusercontent.com/amirhosseinchoghaei/GoogleWifiLED/refs/heads/main/choe.sh
+sleep 1
+chmod +x choe.sh
+wget https://raw.githubusercontent.com/amirhosseinchoghaei/GoogleWifiLED/refs/heads/main/gogo.sh
+sleep 1
+chmod +x gogo.sh
+wget https://raw.githubusercontent.com/amirhosseinchoghaei/GoogleWifiLED/refs/heads/main/led.sh
+sleep 1
+chmod +x led.sh
+wget https://raw.githubusercontent.com/amirhosseinchoghaei/GoogleWifiLED/refs/heads/main/test.sh
+sleep 1
+chmod +x test.sh
+
+TARGET_LINE="/usr/local/LED/led.sh &"
+RC_LOCAL="/etc/rc.local"
+
+grep -Fxq "$TARGET_LINE" "$RC_LOCAL"
+
+if [ $? -ne 0 ]; then
+
+    sed -i '/^exit 0$/i '"$TARGET_LINE" "$RC_LOCAL"
+    echo "done"
+else
+    echo "duplicate"
+fi
+
+
